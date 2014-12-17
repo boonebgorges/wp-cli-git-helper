242924d58b87e9336e9234a604b59b351b1815da
Boone B Gorges <boonebgorges@gmail.com>
2014-12-16 21:14:24 -0500

    Initial commit.

---
+++ readme.md
@@ -0,0 +1,68 @@
+# wp-cli-git-helper
+
+Adds the `wp gh` command, which is a wrapper for `wp [plugin|theme] [update|install]`.
+
+## Huh?
+
+One common workflow for managing a WordPress site through version control is to keep the entire codebase - WP, themes, and plugins - under version control. On this setup, updating third-party plugins and themes can be tedious, since updates may be frequent, requiring lots of commits and commit messages. `wp gh` does this work for you, by composing a commit message and performing the commit.
+
+## Examples
+
+Installing a plugin:
+
+    $ wp gh plugin install --version=3.0 jetpack
+    Installing Jetpack by WordPress.com (3.0)
+    Downloading install package from https://downloads.wordpress.org/plugin/jetpack.3.0.zip...
+    Unpacking the package...
+    Installing the plugin...
+    Plugin installed successfully.
+
+    # `wp gh` has written a commit message and created a changeset
+    $ git log -n 1
+    commit 40588985cc6ef1904350932106737be933b141ce
+    Author: Boone B Gorges <boonebgorges@gmail.com>
+    Date:   Tue Dec 16 21:03:46 2014 -0500
+
+        Install plugin: jetpack.
+
+        Name: Jetpack by WordPress.com
+        Version: 3.0
+
+Updating a theme:
+
+    $ wp gh theme update make
+    Downloading update from https://downloads.wordpress.org/theme/make.1.4.6.zip...
+    Unpacking the update...
+    Installing the latest version...
+    Removing the old version of the theme...
+    Theme updated successfully.
+    Success: Updated 1/1 themes.
+
+    $ wp gh theme update make
+    Installing Jetpack by WordPress.com (3.0)
+    Downloading install package from https://downloads.wordpress.org/plugin/jetpack.3.0.zip...
+    Unpacking the package...
+    Installing the plugin...
+    Plugin installed successfully.
+
+    # `wp gh` has written a commit message and created a changeset
+    $ git log -n 1
+    commit e0327c9bc26fa0e35e1a06d128f998d86c3b81db
+    Author: Boone B Gorges <boonebgorges@gmail.com>
+    Date:   Tue Dec 16 21:09:11 2014 -0500
+
+        Update theme: make.
+
+        Name: Make
+        New version: 1.4.6
+        Previous version: 1.0.0
+
+## Totally Sweet and Customizable Features
+
+None to speak of. This is a tool I've built for my own workflow. It assumes that you keep your entire WP installation in the same repo. It assumes that you like the commit message format I've chosen. It does not support core updates (yet). Some things I will eventually add:
+
+* Ability to override the commit message, either by passing it directly as a `--commit-message` param, or perhaps by drafting a commit message and then sending you to `$EDITOR`.
+* Support for `wp core update`.
+* Proper `delete` support.
+* Support for the `--all` flag.
+* SVN support (jk!)
---
+++ wp-cli-git-helper.php
@@ -0,0 +1,127 @@
+<?php
+
+// Bail if WP-CLI is not present.
+if ( !defined( 'WP_CLI' ) ) return;
+
+require( __DIR__ . '/vendor/kbjr/Git.php/Git.php' );
+
+class Git_Helper_Command extends WP_CLI_Command {
+	public function __invoke( $args, $assoc_args ) {
+		$command = isset( $args[0] ) ? $args[0] : '';
+
+		if ( ! in_array( $command, array( 'plugin', 'theme' ) ) ) {
+			WP_CLI::error( '\'wp gh\' can only be run with \'plugin\' or \'theme\' commands.' );
+		}
+
+		$action = isset( $args[1] ) ? $args[1] : '';
+
+		if ( ! in_array( $action, array( 'update', 'install' ) ) ) {
+			WP_CLI::error( '\'wp gh ' . $command . '\' can only be run with \'update\' or \'install\' commands.' );
+		}
+
+		// Convert 'all' to a list to make formatting easier.
+		if ( isset( $assoc_args['all'] ) ) {
+			// @todo
+		} else {
+			$asset_names = array_slice( $args, 2 );
+		}
+
+		// Grab a list of existing assets.
+		$old_assets = $this->get_assets( $asset_names, $command );
+
+		WP_CLI::run_command( $args, $assoc_args );
+
+		// Verify that the install/update worked.
+		$new_assets = $this->get_assets( $asset_names, $command );
+
+		// Set up values that vary between plugins and themes.
+		if ( 'plugin' === $command ) {
+			$dir_base = WP_CONTENT_DIR . '/plugins/';
+			$message_formats = array(
+				'install' => "Install plugin: %s.\n\nName: %s\nVersion: %s",
+				'update'  => "Update plugin: %s.\n\nName: %s\nNew version: %s\nPrevious version: %s",
+			);
+
+		} else if ( 'theme' === $command ) {
+			$dir_base = WP_CONTENT_DIR . '/themes/';
+			$message_formats = array(
+				'install' => "Install theme: %s.\n\nName: %s\nVersion: %s",
+				'update'  => "Update theme: %s.\n\nName: %s\nNew version: %s\nPrevious version: %s",
+			);
+		}
+
+		// Perform git actions.
+		// @todo 'delete' will require different logic.
+		// @todo Repo at other location than ABSPATH
+		$repo = Git::open( ABSPATH );
+
+		foreach ( $new_assets as $new_asset_name => $new_asset ) {
+			$repo->add( $dir_base . $new_asset_name );
+
+			// @todo override message generation with assoc_arg or with config.yml
+			$message_args = array(
+				$new_asset_name,
+				$new_asset['Name'],
+				$new_asset['Version'],
+			);
+
+			if ( 'update' === $action ) {
+				$message_args[] = $old_assets[ $new_asset_name ]['Version'];
+			}
+
+			$message = vsprintf( $message_formats[ $action ], $message_args );
+
+			$repo->commit( $message );
+		}
+	}
+
+	protected function get_assets( $asset_names, $type ) {
+		$assets = array();
+		foreach ( $asset_names as $asset_name ) {
+			$asset = $this->get_asset_details( $asset_name, $type );
+			if ( $asset ) {
+				$assets[ $asset_name ] = $asset;
+			}
+		}
+
+		return $assets;
+	}
+
+	protected function get_asset_details( $asset_name, $type ) {
+		if ( 'plugin' === $type ) {
+			return $this->get_plugin_details( $asset_name );
+		} else if ( 'theme' === $type ) {
+			return $this->get_theme_details( $asset_name );
+		}
+
+		return false;
+	}
+
+	private function get_plugin_details( $plugin_name ) {
+		foreach ( get_plugins() as $file => $_ ) {
+			if ( $file === "$plugin_name.php" ||
+				( dirname( $file ) === $plugin_name && $plugin_name !== '.' ) ) {
+				$_['file'] = $file;
+				return $_;
+			}
+		}
+	}
+
+	private function get_theme_details( $theme_name ) {
+		$_theme = wp_get_theme( $theme_name );
+
+		if ( ! $_theme->exists() ) {
+			return false;
+		}
+
+		// Format like a theme, because barf.
+		$theme = array(
+			'Name' => $_theme->get( 'Name' ),
+			'Version' => $_theme->get( 'Version' ),
+		);
+
+		return $theme;
+	}
+}
+
+WP_CLI::add_command( 'gh', 'Git_Helper_Command' );
